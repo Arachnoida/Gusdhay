b3456f114d4c52c5a758f3e3ab8d49d4ab66a825
Ida Bagus Gede Dhananjaya <146846530+Arachnoida@users.noreply.github.com>
2023-11-05 19:07:49 +0800

    Add files via upload

---
+++ NilaiArray.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    float nilai[7]={2.5, 1.7, 3.0, 4.2, 3.1, 7.3, 2.5};
+    int length = sizeof(nilai) / sizeof(nilai[0]);
+    float nilai_terkecil = nilai[0];
+    int index = 0;
+
+    for (index = 0; index < 7; index++)
+    {
+        if (nilai_terkecil > nilai[index])
+        {
+            nilai_terkecil = nilai[index];
+        }
+    }
+    printf("\t\n================================================================");
+    printf("\t\n||        Program Menentukan Nilai Terkecil Dalam Array       ||");
+    printf("\t\n================================================================");
+       
+    printf("\t\nDari nilai array = ");
+   for (int index = 0; index < length; index++) {
+        printf("%.1f | ", nilai[index]);
+    }
+    printf("\t\n================================================================");
+    printf("\t\nNilai terkecil dalam array tersebut adalah : %.1f", nilai_terkecil);
+    printf("\t\n================================================================");
+    return 0;
+}
